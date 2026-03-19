6f755268371604175dfac7f1f099a6c5ee296a01
Moritz T <mositho97@gmail.com>
2026-03-19 16:23:07 +0100

    format

--- src/filters/shared.ts
+++ src/filters/shared.ts
@@ -1,7 +1,18 @@
 export type { BuildProfile, BuildSpecificOptions } from "./shared/sections/helpers"
 export { joinSections, optionalSection } from "./shared/sections/helpers"
+export { filterDefaults } from "./shared/defaults"
 export { SOUND_FOLDER, filterStyles, iconMixin, soundFile, styleMixin } from "./shared/styles"
 export { currency, scrolls } from "./shared/sections/currency"
-export { earlyActs, earlySocketFallbacks, twilightStrand } from "./shared/sections/early"
-export { chromaticItems, flasks, highlightedEquipment, jewellery, links, rareItems, sixSockets, socketBases, tinctures } from "./shared/sections/equipment"
+export { early, earlySocketFallbacks, twilightStrand } from "./shared/sections/early"
+export {
+  chromaticItems,
+  flasks,
+  highlightedEquipment,
+  jewellery,
+  links,
+  rareItems,
+  sixSockets,
+  socketBases,
+  tinctures,
+} from "./shared/sections/equipment"
 export { divinationCards, gems, misc, questItems, uniques } from "./shared/sections/global"
